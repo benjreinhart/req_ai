325e5fb29cbfc36303f000733466357f6f8893ea
Ben Reinhart <benjreinhart@gmail.com>
2026-09-21 23:18:35 -0700

    Bump version to 0.0.1-alpha.2

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ReqAI.MixProject do
   use Mix.Project
 
-  @version "0.0.1-alpha.1"
+  @version "0.0.1-alpha.2"
   @source_url "https://github.com/benjreinhart/req_ai"
 
   def project do
